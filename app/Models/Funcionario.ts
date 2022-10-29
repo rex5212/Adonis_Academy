f227afc259016189194a3cfc3d1ba2cfede3070f
rex5212 <xrexv158925lpo@gmail.com>
2022-10-29 20:38:41 -0300

    Academy_Project

--- app/Models/Funcionario.ts
+++ app/Models/Funcionario.ts
@@ -6,7 +6,7 @@ export default class Funcionario extends BaseModel {
   public id: number
 
   @column()
-  public cpf: number
+  public cpf: string
 
   @column()
   public nome: string
--- database/migrations/1663526523010_funcionarios.ts
+++ database/migrations/1663526523010_funcionarios.ts
@@ -6,7 +6,7 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('cpf').notNullable()
+      table.string('cpf').notNullable().unsigned()
       table.string('nome', 60).notNullable()
       table.string('sexo', 1)
       table.string('endereco', 50)
