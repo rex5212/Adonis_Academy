23f7e41c1645ed8abcb4cdb6c831d75d9be2dfc9
Jõao Eduardo <joaoeduardo.lsilva@gmail.com>
2022-11-17 20:00:56 -0300

    atualizado

--- app/Validators/DietaValidator.ts
+++ app/Validators/DietaValidator.ts
@@ -1,38 +1,28 @@
-import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, CustomMessages, rules } from "@ioc:Adonis/Core/Validator";
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
 export default class DietaValidator {
   constructor(protected ctx: HttpContextContract) {}
 
- 
   public schema = schema.create({
-
     funcionarioId: schema.number([
-      rules.exists({ table: 'funcionarios', column: 'id' })
+      rules.exists({ table: "funcionarios", column: "id" }),
     ]),
 
     clienteId: schema.number([
-      rules.exists({ table: 'clientes', column: 'id' })
+      rules.exists({ table: "clientes", column: "id" }),
     ]),
 
     treinamentoId: schema.number([
-      rules.exists({ table: 'treinamentos', column: 'id' })
+      rules.exists({ table: "treinamentos", column: "id" }),
     ]),
 
-    alimento: schema.string([
-      rules.minLength(10000)
-    ]),
+    alimento: schema.string([rules.maxLength(10000)]),
 
-    quantidade: schema.number([
-      rules.unsigned()
-    ]),
+    quantidade: schema.number([rules.unsigned()]),
 
-    complemento: schema.string([
-      rules.maxLength(55)
-    ])
-    
-  })
+    complemento: schema.string([rules.maxLength(1000)]),
+  });
 
-
-  public messages: CustomMessages = {}
+  public messages: CustomMessages = {};
 }
